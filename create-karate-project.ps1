342bbc38f442516610dd06e0e2b21371f7d9433a
thesarfo <ernestsarfo100@gmail.com>
2025-05-09 17:49:46 +0000

    chore: bootstrap project

---
+++ create-karate-project.ps1
@@ -0,0 +1,7 @@
+mvn archetype:generate `
+  -DarchetypeGroupId=dev.thesarfo.karate `
+  -DarchetypeArtifactId=karate-archetype `
+  -DarchetypeVersion=1.5.1 `
+  -DgroupId=dev.thesarfo `
+  -DartifactId=myproject `
+  -DinteractiveMode=false
